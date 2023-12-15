fe0e0fde66429472881ff6eb32e0bb722c8593a1
Sofia Valverde <sofiavalverdevb@gmail.com>
2023-12-15 10:49:34 -0300

    Initial commit

---
+++ secretsanta.py
@@ -0,0 +1,21 @@
+from utils import *
+
+def main():
+
+    while True:
+        print("############################")
+        print("1 - Adicionar participante\n2- Mostrar participantes\n3- Executar sorteio")
+        print("############################")
+        #User's choice#
+        choice = get_choice()
+
+        if choice == "1":
+            get_participant()
+        elif choice == "2":
+            display_participants(participants)
+        elif choice == "3":
+            secret_santa(participants)
+            break
+
+if __name__ == "__main__":
+    main()
