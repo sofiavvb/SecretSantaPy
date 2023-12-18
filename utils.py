17d3c53adb143ba7e28792acc07950485fa2ad75
Sofia Valverde <sofiavalverdevb@gmail.com>
2023-12-18 10:16:58 -0300

    Adiciona a funcionalidade do sorteio e do envio de email

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import random
+import smtplib
+from email.message import EmailMessage
+from config import *
 
 #name: (email, sorteado)
 participants = {}
@@ -15,10 +18,10 @@ def get_choice():
                 print(error)
 
 def get_participant():
-    name = input("Digite o nome: ").replace(" ", "").lower()
+    name = input("Digite o nome: ").trim().lower()
     #ver em como garantir que o email é válido
-    email = input("Digite o email: ").replace(" ", "").lower()
-    participants[name] = email, None
+    email = input("Digite o email: ").trim().lower()
+    participants[name] = [email, None]
 
 def display_participants(participants):
     print("------------------------------------")
@@ -26,22 +29,33 @@ def display_participants(participants):
         print(f"Nome: {person} - Email: {participants[person][0]}")
     print("------------------------------------")
 
-def secret_santa(participants):
-    control = []
-    i = 0
-    #copia os participantes para uma lista de controle
-    for participante in participants:
-        control[i] = participante
-        i += 1
+def send_email(participante, email, amigo_secreto):
+    msg = EmailMessage()
+    msg['Subject'] = 'Amigo Secreto'
+    msg['From'] = EMAIL_HOST
+    msg['To'] = email
+    msg.set_content(f'Olá {participante}! Seu amigo secreto é {amigo_secreto}! Divirta-se!')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_HOST, SENHA)
+        smtp.send_message(msg)
     
+    
+def secret_santa(participants):
+    #copia os participantes do amigo secreto
+    control = list(participants.keys())
+
     for participante in participants:
         escolheu = False
-        aux = random.randint(0, len(control) - 1)
 
         while not escolheu:
+            aux = random.randint(0, len(control) - 1)
             sorteado = control[aux]
 
-            if sorteado != participante and sorteado != 0:
+            if sorteado != participante and sorteado != None:
                 escolheu = True
                 participants[participante][1] = sorteado
-    
+                control[aux] = None
+
+    for participante in participants:
+        send_email(participante, participants[participante][0], participants[participante][1])
